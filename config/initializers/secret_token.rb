f64b155a1e719c220b6221b6d09cd89e71d55e68
Becky Conning <becky.conning@googlemail.com>
2011-11-05 18:25:08 +0000

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gronk::Application.config.secret_token = 'f2a0e8d727f020655b2992855e5860b19a848097a67e284efee1df0c451f3c6016e61b821108b6ac1e4347b09945fc9970260a5e1135d7603d29c21ab53e3863'
